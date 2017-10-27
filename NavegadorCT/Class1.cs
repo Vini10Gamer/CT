3ade357ee13a57232a2aa6e6c50e4e6fa34e8365
BuildTools <unconfigured@null.spigotmc.org>
2017-10-27 20:13:11 -0200

    Adicionar arquivos de projeto.

---
+++ NavegadorCT/Class1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NavegadorCT
+{
+    public  class Class1
+    {
+        enum Destinos {
+            Partida,
+            DC1,
+            DC2,
+            DC3,
+            DC4,
+            DC5,
+            Parada
+        }
+
+    }
+}
